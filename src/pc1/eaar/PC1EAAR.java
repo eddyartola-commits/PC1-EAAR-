44a581606e5b5eaca14659a043a30c98092bfbf2
Eddy <eddyartola@emilianisomascos.edu.gt>
2026-01-21 07:57:31 -0600

    COMMIT MODIFICADO

--- src/pc1/eaar/PC1EAAR.java
+++ src/pc1/eaar/PC1EAAR.java
@@ -14,7 +14,7 @@ public class PC1EAAR {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo en Java");
+        System.out.println("Hola mundo en Java EAAR");
     }
     
 }
